2490a063f2827df5ef3d7a2ffbe0ea6417b0a1cc
OptimusPrime <gulliverraed@gmail.com>
2024-11-20 16:56:24 +0200

    Finished the destiny app, now you can play and see where would yoour choices take you

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = storyBrain.story[0].title
+        choice1Button.setTitle(storyBrain.story[0].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.story[0].choice2, for: .normal)
+        print("Ana et8ayart")
     }
 
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let decison = sender.currentTitle!
+        
+        storyLabel.text = storyBrain.nextStory(userChoice: decison)
+        choice1Button.setTitle(storyBrain.nextChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.nextChoice2(), for: .normal)
+        }
+    
+    
+   
 }
 
